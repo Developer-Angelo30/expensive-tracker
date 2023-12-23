744cc195b94741255b778da568f24a4df9ee6b3e
Developer-Angelo30 <angeloreyes033001@gmail.com>
2023-12-23 22:13:39 +0800

    ui for login page

--- frontend-expensive-tracker/src/components/Index.vue
+++ frontend-expensive-tracker/src/components/Index.vue
@@ -1,3 +1,60 @@
 <template>
-    <section class="" >Hello</section>
+    <section class="t-bg-white t-h-screen t-w-screen t-relative t-overflow-hidden t-select-none" >
+        <div class="" >
+            <span class="t-flex t-justify-center" >
+                <img src="@/assets/welcome.svg" class="t-h-[250px]" >
+            </span>
+            <h4 class="t-font-bold t-text-black/70 t-text-center t-p-0 t-m-0" >WELCOME BACK</h4>
+            <form class="t-p-5" >
+                <div class="mb-3" >
+                    <div class="input-group">
+                        <input v-model="email" type="text" class="form-control" placeholder="Email">
+                        <span class="input-group-text" id="basic-addon2">
+                            <fa icon="envelope"  class="t-text-black/80" ></fa>
+                        </span>
+                    </div>
+                    <small v-if="error_email != ''" class="t-text-red-400" >{{ error_email }}</small>
+                </div>
+                <div class="mb-3" >
+                    <div class="input-group">
+                        <input v-model="password" :type="passwordField.field" class="form-control" placeholder="Password">
+                        <span class="input-group-text" id="basic-addon2">
+                            <fa @click="passwordShow" :icon="passwordField.icon" class="t-text-black/80 t-cursor-pointer" ></fa>
+                        </span>
+                    </div>
+                    <small v-if="error_password != ''" class="t-text-red-400" >{{ error_password }}</small>
+                </div>
+                <button class="btn btn-success w-100 t-uppercase t-font-bold" >
+                    SIGN IN
+                </button>
+                <div class="t-text-[13px] t-text-center t-mt-1" >
+                    Don't have an account?
+                    <router-link to="" class="text-decoration-none t-font-semibold" >Sign Up</router-link>
+                </div>
+            </form>
+        </div>
+    </section>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const email = ref<string>("");
+const password = ref<string>("");
+const error_email = ref<string>("");
+const error_password = ref<string>("");
+
+const passwordField = ref<{ icon: string; field: string }>({
+  icon: "eye",
+  field: "password"
+});
+
+const passwordShow = () => {
+    if(passwordField.value.icon === "eye" && passwordField.value.field === "password"){
+        passwordField.value.field = "text";
+        passwordField.value.icon = "eye-slash"
+    }else{
+        passwordField.value.field = "password";
+        passwordField.value.icon = "eye"
+    }
+};
+</script>
--- frontend-expensive-tracker/src/main.ts
+++ frontend-expensive-tracker/src/main.ts
@@ -2,8 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import {far} from '@fortawesome/free-regular-svg-icons';
+import {fab} from '@fortawesome/free-brands-svg-icons';
+library.add(fas,far,fab);
+
 import App from './App.vue'
 import router from './router'
 
@@ -11,5 +20,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.component('fa',FontAwesomeIcon)
 app.mount('#app')
